6478303eb4160abafa5a2703e54540c465301709
Arnur <arnurarykbayev@mail.ru>
2023-06-24 23:23:11 +0600

    delete pages and add first page

---
+++ src/views/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <b-button variant="warning" to="/emplooyeesList"
+      >The list of employees</b-button
+    >
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomePage",
+  components: {},
+};
+</script>
+
+<style>
+.home {
+  display: flex;
+  flex-direction: column;
+  row-gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
